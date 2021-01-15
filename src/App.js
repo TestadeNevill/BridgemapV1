01edb0cb7c5b86432708e86dfa234e17e0ccde2f
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2021-01-14 21:49:02 -0500

    update folders again

---
+++ src/App.js
@@ -0,0 +1,195 @@
+import React from "react";
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow,
+} from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+} from "use-places-autocomplete";
+
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+} from "@reach/combobox";
+import { formatRelative } from "date-fns";
+
+import "@reach/combobox/styles.css";
+
+import mapStyles from "./mapStyles.js";
+
+const libraries = ["places"];
+
+const mapContainerStyle = {
+    height: "100vh",
+    width: "100vw",
+};
+
+const options = {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true,
+};
+const center = {
+    lat: 41.179192,
+    lng: -73.189484
+};
+
+const APIKEY = "AIzaSyAkjlZ4zHpjrpW2VNAHnrHoLHCN8-ll-4U";
+
+export default function App() {
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: APIKEY,
+        // googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+        libraries,
+    });
+    const [markers, setMarkers] = React.useState([]);
+
+    const [selected, setSelected] = React.useState(null);
+
+    const onMapClick = React.useCallback((event) => {
+        setMarkers((current) => [
+            ...current,
+            {
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng(),
+                time: new Date(),
+            },
+        ]);
+    }, [])
+
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+        mapRef.current = map;
+    }, []);
+
+    const panTo = React.useCallback(({ lat, lng }) => {
+        mapRef.current.panTo({ lat, lng });
+        mapRef.current.setZoom(14);
+    }, []);
+
+    if (loadError) return "Error loading maps";
+    if (!isLoaded) return "Loading Maps";
+
+    return <div>
+        <h1>
+            Testa De Nevill's Bridge {''}<span role="img" aria-label="crown">🗺️</span></h1>
+        <Locate panTo={panTo} />
+        <Search panTo={panTo} />
+        <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            center={center}
+            zoom={12}
+            options={options}
+            onClick={onMapClick}
+            onLoad={onMapLoad}
+        >
+            {markers.map((marker) => (
+                <Marker
+                    key={marker.time.toISOString()}
+                    position={{ lat: marker.lat, lng: marker.lng }}
+                    icon={{
+                        url: '/greenFlag.png',
+                        scaledSize: new window.google.maps.Size(30, 30),
+                        origin: new window.google.maps.Point(0, 0),
+                        anchor: new window.google.maps.Point(15, 15),
+                    }}
+                    onClick={() => {
+                        setSelected(marker);
+                        console.log(marker.lat, marker.lng)
+                    }}
+                />
+            ))}
+            {selected ? (
+                <InfoWindow
+                    position={{ lat: selected.lat, lng: selected.lng }}
+                    onCloseCLick={() => {
+                        setSelected(null);
+                    }}
+                >
+                    <div>
+                        <h2>
+                            New Location!
+                        </h2>
+                        <p> Here! {formatRelative(selected.time, new Date())}</p>
+                    </div>
+                </InfoWindow>
+            ) : null}
+        </GoogleMap>
+    </div>
+        ;
+}
+
+function Locate({ panTo }) {
+    return (
+        <button
+            className="locate"
+            onClick={() => {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        panTo({
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude,
+                        });
+                    },
+                    () => null
+                );
+            }}
+        >
+            <img src="/compass.png" alt="compass" />
+        </button>
+    );
+}
+
+function Search({ panTo }) {
+    const { ready, value, suggestions: { status, data }, setValue, clearSuggestions,
+    } = usePlacesAutocomplete({
+        requestOptions: {
+            location: { lat: () => 41.179192, lng: () => -73.189484 },
+            radius: 200 * 1000,
+        },
+    });
+
+    const handleInput = (e) => {
+        setValue(e.target.value);
+    };
+
+    const handleSelect = async (address) => {
+        setValue(address, false);
+        clearSuggestions();
+
+        try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+        } catch (error) {
+            console.log("😱 Error: ", error);
+        }
+    };
+
+    return (
+        <div className="search">
+            <Combobox onSelect={handleSelect}>
+                <ComboboxInput
+                    value={value}
+                    onChange={handleInput}
+                    disabled={!ready}
+                    placeholder="Search your location"
+                />
+                <ComboboxPopover>
+                    <ComboboxList>
+                        {status === "OK" &&
+                            data.map(({ id, description }) => (
+                                <ComboboxOption key={id} value={description} />
+                            ))}
+                    </ComboboxList>
+                </ComboboxPopover>
+            </Combobox>
+        </div>
+    );
+}
